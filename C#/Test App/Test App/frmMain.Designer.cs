72bc76da9a6d17d1bc2f1800017a5749610c78c2
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-08-02 09:41:38 -0500

    Initial Commit
    This is the first commit. It includes the following
    The main form application and some unfinished code

---
+++ C#/Test App/Test App/frmMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Test_App
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblProduct.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblProduct.Location = new System.Drawing.Point(89, 40);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(2, 23);
+            this.lblProduct.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Guess the Numbers that get this product:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(16, 113);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(154, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(176, 113);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(130, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate Numbers";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblProduct;
+        private Label label2;
+        private Button btnSubmit;
+        private Button btnGenerate;
+    }
+}
---
+++ C#/Test App/Test App/frmMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_App
+{
+    
+    public partial class frmMain : Form
+    {
+         
+
+        public frmMain()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            int numToGuess = rnd.Next(1, 19);
+            int secondNumToGuess = rnd.Next(1, 19);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // this was an accidental event creation. 
+            
+        }
+        private int FindProduct(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+             Random rnd = new Random();
+         int numToGuess = rnd.Next(1, 19);
+         int secondNumToGuess = rnd.Next(1, 19);
+        int product=FindProduct(numToGuess, secondNumToGuess);
+            lblProduct.Text = product.ToString();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // validation code goes here
+            // comparison code goes here
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int numToGuess=random.Next(1, 19);
+            int secondNumToGuess=random.Next(1, 19);
+            int product = FindProduct(numToGuess, secondNumToGuess);
+            lblProduct.Text = product.ToString();
+
+            int secondNumberToGuess=random.Next(1, 19);
+        }
+    }
+}
